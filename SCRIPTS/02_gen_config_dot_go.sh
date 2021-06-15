ae7f439b956b11697ad7a05f377d443071cb6504
0187773933 <collincerbus@student.olympic.edu>
2021-06-15 14:17:04 -0400

    14

--- SCRIPTS/02_gen_config_dot_go.sh
+++ SCRIPTS/02_gen_config_dot_go.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 printf "package config\n\n" > config.go
 printf "var USER_ID = 99\n" >> config.go
-printf "var JUMP_HOST_IP_ADDRESS = \"45.77.156.253\"\n" >> config.go
+printf "var JUMP_HOST_IP_ADDRESS = \"192.168.1.105\"\n" >> config.go
 printf "var JUMP_HOST_SSH_PORT = \"10092\"\n" >> config.go
 
 cp config.go ../GoClientSource/Windows/v1/config/
