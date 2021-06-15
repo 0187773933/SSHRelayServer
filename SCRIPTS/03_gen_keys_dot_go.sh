31241d8eb7f294a57f9007ed3ce98769e0990c33
0187773933 <collincerbus@student.olympic.edu>
2021-06-14 22:56:43 -0400

    10

--- SCRIPTS/03_gen_keys_dot_go.sh
+++ SCRIPTS/03_gen_keys_dot_go.sh
@@ -16,7 +16,8 @@ printf "}\n\n" >> keys.go
 printf "var PRIVATE = []string{\n" >> keys.go
 for number in {1..254}; do
 	PrivateKeyData=$(<../BUILD/KEYS/user${number})
-	SealedPrivateKeyData=$(../SecretBoxBinaries/linux/arm/secretbox --key "$SecretBoxKey" seal message "$PrivateKeyData")
+	../SecretBoxBinaries/linux/arm/secretbox --key "$SecretBoxKey" seal file "../BUILD/KEYS/user${number}" "../BUILD/KEYS/user${number}.sealed"
+	SealedPrivateKeyData=$(<../BUILD/KEYS/user${number}.sealed)
 	printf "\t[]byte(\`$SealedPrivateKeyData\`) ,\n" >> keys.go
 done
 printf "}\n" >> keys.go
